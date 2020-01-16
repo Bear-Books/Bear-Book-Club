e9b13b8bf159a6576be67a31e24de086c0826d82
PEADAR O DONNELL(19-09-2019 13-28 <danu7_op1@danu7>
2020-01-16 14:15:01 +0000

    testing search API for openLibrary. working on returning information we want, ie author_name and title of book. working with isbn and cover URL to return cover jpgs unsuccessfully

---
+++ public/javascripts/search_button.js
@@ -0,0 +1,40 @@
+$(document).ready(
+    function() {
+
+/*
+    $("#search_bar").click(function(){
+        var str = $("#search_box").val();
+        alert(str);
+        $("#results").append("yes");
+    });
+    */
+
+    $("#search_bar").click(function(){
+        $("#results").text("One Moment...");
+        $.ajax({
+            type: 'GET',
+            dataType: "json",
+            url: 'http://openlibrary.org/search.json?q='+$("#search_box").val(),
+            success: function (data) {
+                var posts = "";
+                for(var i = 0; i < data.docs.length; i++){
+                    //alert(data.docs[i].title);
+                    //i += i;<img src="http://covers.openlibrary.org/b/isbn/9780385533225-S.jpg" />
+                    $("#counter").text(i);
+                    posts += data.docs[i].title+"<br/>";
+                    posts += data.docs[i].isbn[0];
+
+                    $.get('http://covers.openlibrary.org/b/isbn/'+data.docs[i].isbn[0]+'-S.jpg').done(function () {
+                        posts += "<img src='http://covers.openlibrary.org/b/isbn/"+data.docs[i].isbn[0]+"-S.jpg'/>";
+                      }).fail(function () {
+                        posts += "<img src='http://i.imgur.com/J5LVHEL.jpg'/>";
+                      });
+                     //posts += "<img src='http://covers.openlibrary.org/b/isbn/"+data.docs[i].isbn[0]+"-S.jpg'/>";            
+                }
+                $("#counter").text(i);
+                $("#results").html(posts);
+            }
+        });
+    });
+
+});
