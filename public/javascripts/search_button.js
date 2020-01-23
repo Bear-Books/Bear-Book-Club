dfb7468a002e733a1046def4eea9a15b656fd7e7
PEADAR O DONNELL(19-09-2019 13-28 <danu7_op1@danu7>
2020-01-23 14:26:54 +0000

    push update

--- public/javascripts/search_button.js
+++ public/javascripts/search_button.js
@@ -17,22 +17,19 @@ $(document).ready(
             url: 'http://openlibrary.org/search.json?q='+$("#search_box").val(),
             success: function (data) {
                 var posts = "";
+                $("#counter").text(data.docs.length);
                 for(var i = 0; i < data.docs.length; i++){
                     //alert(data.docs[i].title);
-                    //i += i;<img src="http://covers.openlibrary.org/b/isbn/9780385533225-S.jpg" />
-                    $("#counter").text(i);
-                    posts += data.docs[i].title+"<br/>";
-                    posts += data.docs[i].isbn[0];
-                    posts += "<img src='http://covers.openlibrary.org/b/isbn/"+data.docs[i].isbn[0]+"-S.jpg'/>";
-                    $.ajax({url:'http://covers.openlibrary.org/b/isbn/'+value.toString(data.docs[i].isbn[0])+'-S.jpg',type:'HEAD',error:do_something});
-                    $.get('http://covers.openlibrary.org/b/isbn/'+data.docs[i].isbn[0]+'-S.jpg').done(function () {
+                    //i += i;<img src="http://covers.openlibrary.org/b/isbn/9780385533225-S.jpg" />   
+                    posts += "<br/><br/>"+data.docs[i].title+"<br/>";                 
+                    posts += data.docs[i].isbn[0]+"<br/>";
+                    if($("img").length){
                         posts += "<img src='http://covers.openlibrary.org/b/isbn/"+data.docs[i].isbn[0]+"-S.jpg'/>";
-                      }).fail(function () {
+                    } else {
                         posts += "<img src='http://i.imgur.com/J5LVHEL.jpg'/>";
-                      });
-                     //posts += "<img src='http://covers.openlibrary.org/b/isbn/"+data.docs[i].isbn[0]+"-S.jpg'/>";            
+                    }     
                 }
-                $("#counter").text(i);
+
                 $("#results").html(posts);
             }
         });
