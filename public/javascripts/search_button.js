322a2cc4b1ad4d4d706b9349ff68a764b84aba03
Rajath Raja(19-09-2019 13-19 <danu7_rr1@danu7>
2020-03-05 14:15:47 +0000

    few changes

--- public/javascripts/search_button.js
+++ public/javascripts/search_button.js
@@ -1,10 +1,6 @@
 $(document).ready(
     function() {
 
-
-        //takes the search bar value and puts it through
-        //open librarys search api. returns title, author and isbn
-        //of the books
     $("#search_button").click(function(){
         $("#results").text("One Moment...");
         var searchBox = "";
@@ -12,40 +8,36 @@ $(document).ready(
         $.ajax({
             type: 'GET',
             dataType: "json",
-            url: 'http://openlibrary.org/search.json?title='+searchBox,
+            url: 'http://openlibrary.org/search.json?q='+searchBox,
             success: function (data) {
 
                 var posts = "";
                 $("#counter").text(data.docs.length);
 
                 //sends back first element of array
-                Object.defineProperty(Array.prototype, 'first', {
+                /*Object.defineProperty(Array.prototype, 'first', {
                     value() {
                       return this.find(e => true);     // or this.find(Boolean)
                     }
-                  });
+                  });*/
 
                   //loop to return title, authors, and first isbn number
                 var d = data.docs;
                 
                     d.forEach((d)=>{ 
                     	if(d.title)
-                    		posts += d.title+"<br/>";
+                             posts += d.title+"<br/>";
 
-                    	
-                                                
                         if(d.author_name){
-
                             for(j in d.author_name){
                                 posts += d.author_name[j]+"<br/>";
                             }
-                        }
+                        }/*
                         if(d.isbn)
                             for(j in d.isbn){
                                 posts += d.isbn[j] + "<br>";
-                            }
+                            }*/
                         posts += "<br><br>";
-                        //posts += d.isbn.first() +"<br/>";
                         
 
                     });
