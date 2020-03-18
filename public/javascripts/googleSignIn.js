74a1c9d8c5897bfacf2cf4fbb8ae38d5807da29d
EVAN KELLY(19-09-2019 13-22 <danu7_ke3@danu7>
2020-03-18 17:59:13 +0000

    Added the sign in to the top corner and deleted unneeded API's

--- public/javascripts/googleSignIn.js
+++ public/javascripts/googleSignIn.js
@@ -1,9 +1,39 @@
-function onSignIn(googleUser) {
+  function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  
+  var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+
+        if(profile === undefined){
+          console.log("user not signed in, no info to return");
+
+        }else{
+          console.log("hey hey hey "+profile.getId());
+          $("#checkLogin").show();
+
+        }
+
+  }
+
+  function returnInfoToScreen(){
+    
+    const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
+  const profile = googleUser.getBasicProfile();
+ 
+    
+  }
+  
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      $("#checkLogin").hide();
+
+    });
   }
 
   
--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,6 @@ var User = require('../models/Users');
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -15,38 +13,6 @@ router.get('/User', function(req, res, next) {
     res.render('User', { title: 'Express' });
   });
 
-router.post('/register', function(req, res, next){
-  var username = req.body.user_name;
-  var password = req.body.password;
-  // Check if account already exists
-  User.findOne({ 'user_name' :  username }, function(err, user)
-  {
-      if (err)
-          res.send(err);
-      // check to see if theres already a user with that email
-      if (user) {
-          res.status(401).json({
-              "status": "info",
-              "body": "Username already taken"
-          });
-      } else {
-          // If there is no user with that username create the user
-          var newUser = new User();
-
-          // set the user's local credentials
-          newUser.user_name = username;
-          newUser.password = newUser.generateHash(password);
-          newUser.access_token = createJwt({user_name:username});
-          newUser.save(function(err, user) {
-              if (err)
-                  throw err;
-     res.cookie('Authorization', 'Bearer ' + user.access_token); 
-              res.json({'success' : 'account created'});
-
-          });
-      }
-  });
-});
 
 /*
  Creates a JWT
@@ -56,44 +22,5 @@ function createJwt(profile) {
       expiresIn: '10d'
   });
 }
-
-
-//login api
-router.post('/login', function(req, res, next){
-  var username = req.body.user_name;
-  var password = req.body.password;
-  User.findOne({'user_name': username}, function (err, user) {
-      // if there are any errors, return the error
-      if (err)
-          res.send(err);
-      // If user account found then check the password
-      if (user) {
-        // Compare passwords
-          if (user.validPassword(password)) {
-              // Success : Assign new access token for the session
-              user.access_token = createJwt({user_name: username});
-              user.save();
-              res.cookie('Authorization', 'Bearer ' + user.access_token); 
-              res.json({'success' : 'loggedIn'});
-          }
-          else {
-              res.status(401).send({
-                  "status": "error",
-                  "body": "Email or password does not match"
-              });
-          }
-      }
-      else
-      {
-          res.status(401).send({
-              "status": "error",
-              "body": "Username not found"
-          });
-      } }); });
-
-
-router.get('/register', function(req, res, next) {
-    res.render('register');
-  });
 
 module.exports = router;
