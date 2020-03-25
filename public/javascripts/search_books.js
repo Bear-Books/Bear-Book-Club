0e0c567d7a144361d615bd616f375486943458e5
DAVID NOONE( 9-03-2019 01-36 <danu7_nd1@danu7>
2020-03-25 21:24:28 +0000

    Almost there

--- public/javascripts/search_books.js
+++ public/javascripts/search_books.js
@@ -2,15 +2,28 @@ $(document).ready(
     function() {
 
         var searchQ = window.location.href.slice(window.location.href.indexOf('?') + 1);
-        $('#results').html(searchQ);
-        var posts = "";
+        var limit = 2;
 
-        $.getJSON('http://openlibrary.org/search.json?title='+searchQ, function(data) {
+        $.getJSON('http://openlibrary.org/search.json?q='+searchQ, function(data) {
 
-            $("#counter").text(data.docs.length +" results returned:");
-            var d = data.docs;
-            d.forEach((d)=>{ 
+            $("#num_results").text(data.numFound);
+            $("#searchQ").text(searchQ.replace("+"," "));
+
+            if (data.numFound < limit) {
+                limit = data.numFound;
+            }
+
+            var books = data.docs;
+
+            /* I need to loop through docs, use edition_count as my filter for the top 20*/
+
+            for (var i=0; i<limit; i++) {
                 
+                $( "#full_search" ).append('<div class="row"><div class="col-md-7"><a href="#"><img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/200x300" alt=""></a></div><div class="col-md-5"><h3 id="author_name">David Noone</h3><p ></p><a class="btn btn-primary" href="#">Add Book To Reading List</a></div></div><hr>');
+            }
+            //d.forEach((d)=>{ 
+                
+                /*
                 if(d.isbn){
                     posts +="<a href = '/Book?isbn="+d.isbn[0]+"'>";
                 }
@@ -28,9 +41,13 @@ $(document).ready(
                         posts += d.isbn[0] + "</a><br>";
                     //}
                 posts += "<br><br>";
-            });
-            $("#results").html(posts);
-
+                */
+            //});
+            //$("#results").html(posts);
+            
+            $("#readingBearCon").remove();
+            $("#full_search").css("opacity", 1);
         });
+        
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/Users');
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,10 +19,55 @@ router.get('/AboutUs', function(req, res, next) {
   res.render('AboutUs', { title: 'Express' });
 });
 
+/* GET Search page and book sorter*/
 router.get('/search', function(req, res, next) {
-  res.render('search', { title: 'Express' });
+
+  var arraySort = require('array-sort');
+  var searchString = req.originalUrl.split("?")[1].toLowerCase();
+  var searchUrl = "http://openlibrary.org/search.json?q=" + searchString;
+  var limit = 20;
+  var top = [];
+  /*
+  http.get(searchUrl, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var bookJSON = JSON.parse(body);
+        var bookObj = bookJSON.docs;
+
+        sortBook = arraySort(bookObj, "isbn");
+        //console.log("Got a response: ", bookObj);
+        for (var i=0; i<limit; i++) {
+          //console.log(sortBook[i]);
+          
+            top.push(sortBook[i]);
+          
+        }
+
+        //console.dir(top.title);
+        for (var i=0; i<limit; i++) {
+          if (top[i].title) 
+            console.dir(top[i].title);
+          if (top[i].author_name) 
+            console.dir(top[i].author_name);
+      }
+        
+    });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+  }); */
+
+  //console.log(searchUrl);
+  
+  res.render('search', { title: 'Express'});
+  
 });
 
+/* GET Users page */
 router.get('/getUsers', function(req, res, next)
 {
     User.find({}, function (err, users) {
