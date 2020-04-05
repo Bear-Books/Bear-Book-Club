a0f5574854235beb05ede471433d3d867df6dd8d
DAVID NOONE( 9-03-2019 01-36 <danu7_nd1@danu7>
2020-04-05 13:49:31 +0100

    changes

--- public/javascripts/search_books.js
+++ public/javascripts/search_books.js
@@ -2,53 +2,45 @@ $(document).ready(
     function() {
 
         var searchQ = window.location.href.slice(window.location.href.indexOf('?') + 1);
-        var limit = 2;
-        var basic = '<div class="row"><div class="col-md-7"><a href="#"><img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/200x300" alt=""></a></div><div class="col-md-5"><h3 id="author_name">David Noone</h3><p ></p><a class="btn btn-primary" href="#">Add Book To Reading List</a></div></div><hr>';
+        var limit = 20;
+        var output1 = '<div class="row"><div class="col-md-7"><a href="#"><img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/200x300" alt=""></a></div><div class="col-md-5"><h3 id="author_name">'
+        var output3 = '</h3><p ></p><a class="btn btn-primary" href="#">Add Book To Reading List</a></div></div><hr>';
+        var author = "";
+        var cover = "Default";
+        var title = "some title";
+        var validBooks = [];
 
-        $.getJSON('http://openlibrary.org/search.json?q='+searchQ, function(data) {
+        $.getJSON('https://openlibrary.org/search.json?q='+searchQ+'&mode=everything&has_fulltext=true', function(data) {
 
             $("#num_results").text(data.numFound);
             $("#searchQ").text(searchQ.replace("+"," "));
-
-            if (data.numFound < limit) {
-                limit = data.numFound;
-            }
-
+  
             var books = data.docs;
 
-            /* I need to loop through docs, use edition_count as my filter for the top 20*/
-
-            for (var i=0; i<limit; i++) {
+            for (var i=0; i<data.numFound; i++) {
                 
-                $( "#full_search" ).append(basic);
+                if (validBooks.length >= limit) {
+                    break;
+                }
+                
+                if (books[i].cover_i) {
+                    validBooks.push(books[i]);
+                }
+                /*if (books[i].author_name) {
+                   author = books[i].author_name.toString();
+                }*/
+
+                //$( "#full_search" ).append(output1 + author + output3);
+
+                //console.dir(author); 
             }
-            //d.forEach((d)=>{ 
-                
-                /*
-                if(d.isbn){
-                    posts +="<a href = '/Book?isbn="+d.isbn[0]+"'>";
-                }
-                if(d.title) {
-                    posts += d.title+"<br/>";
-                }
-                if(d.author_name){
-
-                    for(j in d.author_name){
-                        posts += d.author_name[j]+"<br/>";
-                    }
-                }
-                if(d.isbn)
-                    //for(j in d.isbn){
-                        posts += d.isbn[0] + "</a><br>";
-                    //}
-                posts += "<br><br>";
-                */
-            //});
-            //$("#results").html(posts);
-            
             $("#readingBearCon").remove();
             $("#full_search").css("opacity", 1);
+            console.dir(validBooks);
+            validBooks.sort(function(a, b){
+                return a.isbn.length - b.isbn.length;
+            })
+            console.dir(validBooks);
         });
         
-
 });
